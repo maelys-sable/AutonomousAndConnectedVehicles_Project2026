eba66e23eabc793858d847f652ea45a119c300c8
maelys-sable <maelyssable56000@gmail.com>
2026-06-08 18:18:45 +0200

    Modification of the ports

--- docker_run_client.sh
+++ docker_run_client.sh
@@ -2,8 +2,8 @@ docker run --rm \
         -v $(pwd)/userCode/:/workspace/team_code/ \
         --network=bridge \
         --name carla-client-instance-${USER} \
-        -p TP:TP\
-        -p VP:VP \
+        -p 8833:8833\
+        -p 9833:9833 \
         -d\
         -it carla-client \
         /bin/bash
--- docker_run_server.sh
+++ docker_run_server.sh
@@ -1,1 +1,1 @@
-docker run --rm --gpus '"device=DEV"' --net=bridge -p CP-P2:CP-P2 --name carla_server_${USER} -d carla_leaderboard_2.0:latest /bin/bash ./CarlaUE4.sh -carla-port=CP -RenderOffScreen
+docker run --rm --gpus '"device=0"' --net=bridge -p 6034:6034 --name carla_server_${USER} -d carla_leaderboard_2.0:latest /bin/bash ./CarlaUE4.sh -carla-port=6033 -RenderOffScreen
