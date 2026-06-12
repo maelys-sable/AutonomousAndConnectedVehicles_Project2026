c1fc5f26cbb6b593bb0cc80ffda72b96a2092afb
maelys-sable <maelyssable56000@gmail.com>
2026-06-12 11:44:40 +0200

    Update docker_run_server.sh

--- docker_run_server.sh
+++ docker_run_server.sh
@@ -1,1 +1,1 @@
-docker run --rm --gpus '"device=0"' --net=bridge -p 6034:6034 --name carla_server_${USER} -d carla_leaderboard_2.0:latest /bin/bash ./CarlaUE4.sh -carla-port=6033 -RenderOffScreen
+docker run --rm --gpus '"device=0"' --net=bridge -p 6033:6033 -p 6034:6034 --name carla_server_${USER} -d carla_leaderboard_2.0:latest /bin/bash ./CarlaUE4.sh -carla-port=6033 -RenderOffScreen
