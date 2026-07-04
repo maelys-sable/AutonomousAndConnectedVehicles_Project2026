1d5d92ec97df7877e0843949f2f64d8746fe05ef
maelys-sable <maelyssable56000@gmail.com>
2026-07-04 16:03:36 +0200

    Isolation scénario

--- client_carla/userCode/run_test.sh
+++ client_carla/userCode/run_test.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 #qdtrack_ training.xml
 # export ROUTES=/workspace/team_code/route_controlling.xml
-export ROUTES=/workspace/team_code/route_1_avddiem.xml
+#export ROUTES=/workspace/team_code/route_1_avddiem.xml
 #export ROUTES=${LEADERBOARD_ROOT}/data/routes_devtest.xml
+
+export ROUTES=/workspace/team_code/route_test_parked_7.xml
+export CHECKPOINT_ENDPOINT=/workspace/team_code/results_parked_7.json   # évite d'écraser ta baseline complète
+
 export REPETITIONS=1
 export DEBUG_CHALLENGE=1
 export TEAM_AGENT=/workspace/team_code/carla_behavior_agent/basic_autonomous_agent.py
@@ -11,7 +15,7 @@ export CHALLENGE_TRACK_CODENAME=SENSORS
 export CARLA_HOST=172.16.174.233
 export CARLA_PORT=6033
 export CARLA_TRAFFIC_MANAGER_PORT=8833
-export CHECKPOINT_ENDPOINT=/workspace/team_code/simulation_results_r1.json
+#export CHECKPOINT_ENDPOINT=/workspace/team_code/simulation_results_r1.json
 export DEBUG_CHECKPOINT_ENDPOINT=/workspace/team_code/results/live_results.txt
 export RESUME=false
 export TIMEOUT=60
